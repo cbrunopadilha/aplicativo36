508ee6cc9e51f82aa6c8b465838a6500305826b7
cbrunopadilha <cbrunopadilha@gmail.com>
2014-07-01 02:13:43 -0300

    Create dados-2014-05-28.js

---
+++ dados-2014-05-28.js
@@ -0,0 +1,85 @@
+
+tabelaHorarios.push({
+	"sentido": "POA-ALV", 
+	"linha":"LINHAS TRIANGULO", 
+	"dia":"DIA ÚTIL",
+	"hora":"18:40", 
+	"descricao":"Portoverde via Jd. Algarve"
+});
+tabelaHorarios.push({
+	"sentido": "POA-ALV", 
+	"linha":"LINHAS TRIANGULO", 
+	"dia":"DIA ÚTIL",
+	"hora":"18:10", 
+	"descricao":"Jardim Porto Alegre"
+});
+tabelaHorarios.push({
+	"sentido": "POA-ALV", 
+	"linha":"LINHAS TRIANGULO", 
+	"dia":"DIA ÚTIL",
+	"hora":"18:15", 
+	"descricao":"Jardim Aparecida"
+});
+tabelaHorarios.push({
+	"sentido": "POA-ALV", 
+	"linha":"LINHAS TRIANGULO", 
+	"dia":"DIA ÚTIL",
+	"hora":"18:45", 
+	"descricao":"Jardim Aparecida"
+});
+tabelaHorarios.push({
+	"sentido": "POA-ALV", 
+	"linha":"LINHAS TRIANGULO", 
+	"dia":"DIA ÚTIL",
+	"hora":"18:00", 
+	"descricao":"Passo da Figueira"
+});
+tabelaHorarios.push({
+	"sentido": "POA-ALV", 
+	"linha":"LINHAS TRIANGULO", 
+	"dia":"DIA ÚTIL",
+	"hora":"18:30", 
+	"descricao":"Vila Elza"
+});
+tabelaHorarios.push({
+	"sentido": "POA-ALV", 
+	"linha":"LINHAS TRIANGULO", 
+	"dia":"DIA ÚTIL",
+	"hora":"17:55", 
+	"descricao":"Salomé"
+});
+tabelaHorarios.push({
+	"sentido": "POA-ALV", 
+	"linha":"LINHAS TRIANGULO", 
+	"dia":"DIA ÚTIL",
+	"hora":"18:20", 
+	"descricao":"Salomé"
+});
+tabelaHorarios.push({
+	"sentido": "POA-ALV", 
+	"linha":"LINHAS TRIANGULO", 
+	"dia":"DIA ÚTIL",
+	"hora":"18:50", 
+	"descricao":"Salomé"
+});
+tabelaHorarios.push({
+	"sentido": "POA-ALV", 
+	"linha":"LINHAS TRIANGULO", 
+	"dia":"DIA ÚTIL",
+	"hora":"18:05", 
+	"descricao":"Umbú"
+});
+tabelaHorarios.push({
+	"sentido": "POA-ALV", 
+	"linha":"LINHAS TRIANGULO", 
+	"dia":"DIA ÚTIL",
+	"hora":"18:35", 
+	"descricao":"Umbú"
+});
+tabelaHorarios.push({
+	"sentido": "POA-ALV", 
+	"linha":"LINHAS TRIANGULO", 
+	"dia":"DIA ÚTIL",
+	"hora":"17:45", 
+	"descricao":"Jardim Aparecida"
+});
